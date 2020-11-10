d348de231086df3c536d2f4afc8a7f2f091068eb
Armandoxx123 <josearmando.mendoza@itsn.edu.mx>
2020-11-10 12:02:53 -0600

    se agregaron las rutas de la leccion 1

--- api-pov/app/Http/Controllers/Lesson1Controller.php
+++ api-pov/app/Http/Controllers/Lesson1Controller.php
@@ -1,10 +1,110 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Activity;
+use App\Models\Time;
+use App\Models\Lesson;
+use App\Models\Answer;
+use App\Models\Qualification_activity;
+use App\Models\Qualification_time;
 use Illuminate\Http\Request;
 
 class Lesson1Controller extends Controller
 {
     //
+    function add_answer(Request $request){
+        $request->validate([
+            'lesson_id' => 'required|string',
+            'time_id' => 'required|string',
+            'activity_id' => 'required|string',
+            'answer' => 'required|string',
+        ]);
+        $answer = new Answer();
+        $answer->lesson_id = $request->lesson_id;
+        $answer->time_id = $request->time_id;
+        $answer->activity_id = $request->activity_id;
+        $answer->answer = $request->answer;
+        $answer->save();
+    }
+    function add_lesson(Request $request){
+        $request->validate([
+            'lesson' => 'required|string',
+        ]);
+        $lesson = new Lesson();
+        $lesson->lesson = $request->lesson;
+        $lesson->save();
+        return response([
+            'message' => 'ok',
+        ]);
+    }
+    function add_time(Request $request){
+        $request->validate([
+            'verbalTense' => 'required|string',
+        ]);
+        $time = new Time();
+        $time->verbalTense = $request->verbalTense;
+        $time->save();
+        return response([
+            'message' => 'ok',
+        ]);
+    }
+    function add_activity(Request $request){
+        $request->validate([
+            'name' => 'required|string',
+        ]);
+        $activity = new Activity();
+        $activity->name = $request->name;
+        $activity->save();
+        return response([
+            'message' => 'ok',
+        ]);
+    }
+    function add_qualification_activity(Request $request){
+        $request->validate([
+            'user_id' => 'required|string',
+            'lesson_id'=> 'required|string',
+            'time_id' => 'required|string',
+            'activity_id' => 'required|string',
+            'qualification' => 'required|string',
+        ]);
+        $qualification = new Qualification_activity();
+        $qualification->user_id = $request->user_id;
+        $qualification->lesson_id = $request->lesson_id;
+        $qualification->time_id = $request->time_id;
+        $qualification->activity_id = $request->activity_id;
+        $qualification->qualification = $request->qualification;
+        $qualification->save();
+        return response([
+            'message' => 'ok',
+        ]);
+    }
+    function add_qualification_time(Request $request){
+        $request->validate([
+            'user_id' => 'required|string',
+            'lesson_id'=> 'required|string',
+            'time_id' => 'required|string',
+            'qualification' => 'required|string',
+        ]);
+        $qualification = new Qualification_time();
+        $qualification->user_id = $request->user_id;
+        $qualification->lesson_id = $request->lesson_id;
+        $qualification->time_id = $request->time_id;
+        $qualification->qualification = $request->qualification;
+        $qualification->save();
+        return response([
+            'message' => 'ok',
+        ]);
+    }
+    
+    function show_qualification_activity(Request $request){
+        $qualifications = Qualification_activity::all();
+        return response()->json([
+            'qualifications' => $qualifications,]);
+    }
+    function show_qualification_time(Request $request){
+        $qualifications = Qualification_time::all();
+        return response()->json([
+            'qualifications' => $qualifications,
+        ]);
+    }
 }
--- api-pov/routes/api.php
+++ api-pov/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Lesson1Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,8 +23,16 @@ Route::group(['prefix' => 'auth'], function(){
     Route::put('updateuser/{id}', [AuthController::class, 'updateuser']);
     Route::put('updatepass/{id}', [AuthController::class, 'updatepass']);
     Route::delete('deleteuser/{id}', [AuthController::class, 'deleteuser']);
+    Route::post('add_answer', [Lesson1Controller::class, 'add_answer']);
+    Route::post('add_lesson', [Lesson1Controller::class, 'add_lesson']);
+    Route::post('add_time', [Lesson1Controller::class, 'add_time']);
+    Route::post('add_activity', [Lesson1Controller::class, 'add_activity']);
     Route::group(['middleware' => 'auth:api'], function(){
         Route::get('logout', [AuthController::class, 'logout']);
         Route::get('user', [AuthController::class, 'user']);
+        Route::post('add_qualification_activity',[Lesson1Controller::class, 'add_qualification_activity']);
+        Route::post('add_qualification_time',[Lesson1Controller::class, 'add_qualification_time']);
+        Route::get('show_qualification_time',[Lesson1Controller::class, 'show_qualification_time']);
+        Route::get('show_qualification_activity',[Lesson1Controller::class, 'show_qualification_activity']);
     });
 });
